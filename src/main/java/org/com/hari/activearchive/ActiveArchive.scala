0f567e9ae7b13731ff976c02ec13a8e7c601ad05
Hari <haryramesh@hotmail.com>
2016-07-08 16:23:50 -0400

    commit..

--- src/main/java/org/com/hari/activearchive/ActiveArchive.scala
+++ src/main/java/org/com/hari/activearchive/ActiveArchive.scala
@@ -5,11 +5,22 @@ import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.types.{ StructType, DateType, StringType, StructField, DoubleType }
 import org.apache.spark.SparkConf
 import scala.io.Source
-import java.io.File
 import org.apache.spark.sql.functions._
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import org.apache.hadoop.conf._
+import org.apache.hadoop.fs._
+import org.apache.hadoop.fs.permission.FsPermission
 
 object ActiveArchive {
-
+  private val conf = new Configuration()
+  private val hdfsCoreSitePath = new Path("core-site.xml")
+  private val hdfsHDFSSitePath = new Path("hdfs-site.xml")
+  conf.addResource(hdfsCoreSitePath)
+  conf.addResource(hdfsHDFSSitePath)
+  private val fileSystem = FileSystem.get(conf)
   def main(args: Array[String]): Unit = {
 
     val dataFile = args(0)
@@ -18,14 +29,14 @@ object ActiveArchive {
     val tableName = args(3)
     val partName = args(4)
     val loadEvent: String = args(5)
-    val tblPath = "/tmp/ramesh2/" + projectName + "/" + tableName + "/" 
-    val opPath = tblPath + partName + "/" + loadEvent+"/"
+    val tblPath = "/user/ramesh2/" + projectName + "/" + tableName + "/"
+    val opPath = tblPath + partName + "/" + loadEvent + "/"
     val conf = new SparkConf().setAppName("Active Archive")
     val sc = new SparkContext(conf)
     val sqlCont = new HiveContext(sc)
-    val delimiter = getDelimiter(modelFile)
-    val headerOption = getHeaderOption(modelFile)
-    val schema = getSchema(modelFile)
+    val delimiter = getDelimiter(modelFile, sc)
+    val headerOption = getHeaderOption(modelFile, sc)
+    val schema = getSchema(modelFile, sc)
     val myData = sqlCont.read.format("com.databricks.spark.csv")
       .option("delimiter", delimiter)
       .option("header", headerOption)
@@ -40,69 +51,80 @@ object ActiveArchive {
       colString += x.name + " " + x.dataType.typeName + ","
     }
     val formattedColString = colString.dropRight(1)
-
-    //    
-    //    sqlCont.setConf("hive.exec.dynamic.partition", "true")
-    //    sqlCont.setConf("hive.exec.dynamic.partition.mode", "nonstrict")
     val dbString = "CREATE  DATABASE IF NOT EXISTS " + projectName
     sqlCont.sql(dbString)
     sqlCont.sql("use " + projectName)
-    val tblString = "CREATE EXTERNAL TABLE IF NOT EXISTS " + tableName + "(" + formattedColString + ")PARTITIONED BY(batchdate STRING, loadeventid STRING) STORED AS PARQUET LOCATION '"+tblPath+"'" 
+    val tblString = "CREATE EXTERNAL TABLE IF NOT EXISTS " + tableName + "(" + formattedColString + ")PARTITIONED BY(batchdate STRING, loadeventid STRING) STORED AS PARQUET LOCATION '" + tblPath + "'"
     sqlCont.sql(tblString)
-    myData.write.mode("overwrite").parquet(opPath)
+    mkdirs(tblPath)
+    chmod(tblPath)
+    myData.write.mode("overwrite").parquet(opPath)    
+    chmod(opPath)
+    val dropString = "ALTER TABLE " + tableName + " DROP IF EXISTS PARTITION (batchdate='" + partName + "', loadeventid='" + loadEvent + "')"
+    sqlCont.sql(dropString)
     val partString = "ALTER TABLE " + tableName + " ADD IF NOT EXISTS PARTITION (batchdate='" + partName + "', loadeventid='" + loadEvent + "') location '" + opPath + "'"
-    sqlCont.sql(partString)    
+    sqlCont.sql(partString)
     val hiveQL = sqlCont.read.table(tableName)
     hiveQL.show()
     println(hiveQL.count())
     hiveQL.printSchema()
+    sc.stop()
 
   }
 
-  def getDelimiter(modelFileName: String): String = {
+  def getDelimiter(modelFileName: String, sc: SparkContext): String = {
     var delimiter = ","
-    if (new java.io.File(modelFileName).exists) {
-      for (line <- Source.fromFile(modelFileName).getLines().take(1)) {
-        var fileArray = line.split(":")
-        if (fileArray.size.>=(1) && fileArray(0).toLowerCase().trim() == "delimiter") {
-          delimiter = fileArray(1)
-        }
+    var path = new Path(modelFileName)
+    if (fileSystem.exists(path)) {
+      val delimData = sc.textFile(modelFileName, 1).first()
+      val delimiterVar = delimData.split(":")
+      if (delimiterVar(0).toLowerCase().trim() == "delimiter") {
+        delimiter = delimiterVar(1)
       }
     }
+    println("Delimiter is: "+delimiter)
     return delimiter
   }
 
-  def getHeaderOption(modelFileName: String): String = {
+  def getHeaderOption(modelFileName: String, sc: SparkContext): String = {
     var header = "false"
-    if (new java.io.File(modelFileName).exists) {
-      var i = 0;
-      for (line <- Source.fromFile(modelFileName).getLines().take(2)) {
-        i += 1
-        if (i == 2) {
-          var fileArray = line.split(":")
-          if (fileArray.size.>=(1) && fileArray(0).toLowerCase().trim() == "header") {
-            header = fileArray(1)
-          }
-        }
+    var path = new Path(modelFileName)
+    if (fileSystem.exists(path)) {
+      val delimData = sc.textFile(modelFileName, 1).take(2).last
+      val delimiterVar = delimData.split(":")
+      if (delimiterVar(0).toLowerCase().trim() == "header") {
+        header = delimiterVar(1)
       }
     }
+    println("Delimiter is: "+header)
     return header
   }
 
-  def getSchema(modelFileName: String): StructType = {
+  def getSchema(modelFileName: String, sc: SparkContext): StructType = {
+    println("Info: getting Schema from model file")
+    var path = new Path(modelFileName)
     var schema = StructType(Array(StructField("Column00", StringType, false)))
-    if (new java.io.File(modelFileName).exists) {
-      var i = 0;
-      for (line <- Source.fromFile(modelFileName).getLines().take(3)) {
-        i += 1
-        if (i == 3) {
-          val schemaLine = line //+ ":batchdate:loadeventid"
-          schema = StructType(
+    if (fileSystem.exists(path)) {
+      val schemaLine = sc.textFile(modelFileName, 1).take(3).last
+      schema = StructType(
             schemaLine.split(":").map(fieldName => StructField(fieldName.toLowerCase(), StringType, false)))
-        }
-      }
     }
+    println(schema)
     return schema
+  }
+
+  def chmod(folderPath: String): Unit = {
+    var path = new Path(folderPath)
+    if (fileSystem.exists(path)) {
+      fileSystem.setPermission(path, FsPermission.valueOf("drwxrwxrwx"))
+    }
+  }
+  
+  def mkdirs(folderPath: String): Unit = {
+   var path = new Path(folderPath)
+     if (!fileSystem.exists(path)) {
+    fileSystem.mkdirs(path,FsPermission.valueOf("drwxrwxrwx"))
+     }
   }
 
 }
